8821a512f2a47c034463c730dab263c33648603a
skyskyskyha <skyskyha@umich.edu>
2023-02-19 20:34:29 +0800

    finish ScorePanel class

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
 import "./index.less"
-
-class Food {
-    element: HTMLElement;
-    constructor() {
-        this.element = document.getElementById("food")!;
-
-    }
-    get X(){
-        return this.element.offsetLeft;
-    }
-    get Y(){
-        return this.element.offsetTop;
-    }
-
-    change() {
-        let top = Math.round(Math.random()*29)*10;
-        let left = Math.round(Math.random()*29)*10;
-        this.element.style.left=left+'px';
-        this.element.style.top=top+'px';
-    }
-}
-
+import Food from "./modules/Food";
 
 const food = new Food();
+
 
 
